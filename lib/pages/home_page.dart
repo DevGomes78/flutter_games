50f6c56d9722529d9efa80d05c15196949370a05
usuario <gomes.amilson@gmail.com>
2022-11-18 02:48:05 -0300

    adicionado body no arquivo home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,14 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_games/pages/game_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
+    return const Scaffold(
+   body: GamePage(),
     );
   }
 }
