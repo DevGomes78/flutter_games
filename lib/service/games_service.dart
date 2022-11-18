a9309b39706460633f9571b8dbf871d0097b4c24
usuario <gomes.amilson@gmail.com>
2022-11-18 04:16:58 -0300

    ´criado função getData1 para arquivo game_service.dart

--- lib/service/games_service.dart
+++ lib/service/games_service.dart
@@ -6,11 +6,19 @@ import 'package:http/http.dart' as http;
 
 class GameService extends ChangeNotifier {
   List<Results> lista =[];
+  List<Results> lista1 =[];
   Future<List<Results>> getData() async {
     final response = await http
         .get(Uri.parse('https://videogamesapi.herokuapp.com/api/games/'));
      var decodejson = jsonDecode(response.body);
      decodejson['results'].forEach((item)=> lista.add(Results.fromJson(item)));
      return lista;
+  }
+  Future<List<Results>> getData1() async {
+    final response = await http
+        .get(Uri.parse('https://videogamesapi.herokuapp.com/api/games/?page=2'));
+    var decodejson = jsonDecode(response.body);
+    decodejson['results'].forEach((item)=> lista1.add(Results.fromJson(item)));
+    return lista1;
   }
 }
