5f3758c2690fae9382c0c7f3586739f89e14a127
usuario <gomes.amilson@gmail.com>
2022-11-18 02:46:37 -0300

    adicionado multiprovider no arquivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_games/pages/home_page.dart';
+import 'package:flutter_games/service/games_service.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context)=> GameService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          brightness: Brightness.dark
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
