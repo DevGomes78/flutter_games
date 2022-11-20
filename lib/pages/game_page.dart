953b9c4f9d823300e6e3045c5c306dad38ddd24a
usuario <gomes.amilson@gmail.com>
2022-11-20 04:03:08 -0300

    refatorado codigo do arquivo game_page.dart

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_games/components/game_list_popular.dart';
-import 'package:flutter_games/service/games_service.dart';
-import 'package:provider/provider.dart';
-
-import '../components/game-list.dart';
 import '../components/game_list1.dart';
-import '../components/list_console.dart';
 import '../components/top_card.dart';
 
 class GamePage extends StatefulWidget {
@@ -19,22 +13,31 @@ class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: const [
-        TopCard(),
-        SizedBox(height: 10),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Text(
-            'Top this year',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.blueAccent, Colors.black])),
+        child: ListView(children: const [
+          TopCard(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'Top this year',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
           ),
-        ),
-        GameList1(),
-      ]),
+          SizedBox(height: 10),
+          GameList1(),
+        ]),
+      ),
     );
   }
 }
