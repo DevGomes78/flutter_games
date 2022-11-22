d3825648aabb4b7d3659d20de751c464cb80b9ff
usuario <gomes.amilson@gmail.com>
2022-11-22 04:43:56 -0300

    atualizado details_page.dart

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -22,75 +22,110 @@ class _DetailsPageState extends State<DetailsPage> {
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
                 colors: [Colors.blueAccent, Colors.black])),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height / 2.5,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: Colors.transparent),
-              child: ClipRRect(
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-                child: Image.network(
-                  widget.results!.cover.toString(),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
                 children: [
-                  Text(
-                    widget.results!.releaseDate.toString().substring(0, 4),
-                    style: const TextStyle(fontSize: 18, color: Colors.grey),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 2.2,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Colors.transparent),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                      child: Image.network(
+                        widget.results!.cover.toString(),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
                   ),
-                  const SizedBox(width: 10),
-                  const Text('|'),
-                  const SizedBox(width: 10),
-                  Text(
-                    widget.results!.publisher.toString(),
-                    style: const TextStyle(fontSize: 18, color: Colors.grey),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 2.2,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      gradient: const LinearGradient(
+                        colors: [Colors.black54, Colors.transparent],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
                   ),
-                  const SizedBox(width: 10),
-                  const Text('|'),
-                  const SizedBox(width: 10),
-                  const Icon(
-                    Icons.star,
-                    size: 18,
-                    color: Colors.amber,
-                  ),
-                  const SizedBox(width: 10),
-                  const Text(
-                    '8.5',
-                    style: TextStyle(fontSize: 18, color: Colors.grey),
-                  ),
+                  Positioned(
+                      left: 5,
+                      top: 20,
+                      child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white38),
+                          child: IconButton(
+                            onPressed: (){Navigator.pop(context);},
+                            icon: const Icon(
+                              Icons.arrow_back_ios,
+                              size: 25,
+                              color: Colors.white,
+                            ),
+                          )))
                 ],
               ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Text(
-                'Description',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  children: [
+                    Text(
+                      widget.results!.releaseDate.toString().substring(0, 4),
+                      style: const TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                    const SizedBox(width: 10),
+                    const Text('|'),
+                    const SizedBox(width: 10),
+                    Text(
+                      widget.results!.publisher.toString(),
+                      style: const TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                    const SizedBox(width: 10),
+                    const Text('|'),
+                    const SizedBox(width: 10),
+                    const Icon(
+                      Icons.star,
+                      size: 18,
+                      color: Colors.amber,
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      '4.5',
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Text(widget.results!.description.toString()),
-            )
-          ],
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  'Description',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(widget.results!.description.toString()),
+              )
+            ],
+          ),
         ),
       ),
     );
