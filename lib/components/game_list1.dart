419d3d1ebbc45b402a3911ee832ffc03c4643ff1
usuario <gomes.amilson@gmail.com>
2022-11-24 01:06:19 -0300

    criado funçao gameList

--- lib/components/game_list1.dart
+++ lib/components/game_list1.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_games/pages/details_page.dart';
 import 'package:provider/provider.dart';
+import '../constants/string_constants.dart';
 import '../service/games_service.dart';
 
 class GameList1 extends StatefulWidget {
@@ -39,124 +40,113 @@ class _GameList1State extends State<GameList1> {
             default:
               if (snapshot.hasError) {
                 return const Center(
-                  child: Text('StringConstants.errorPage'),
+                  child: Text(StringConstants.errorPage),
                 );
               } else {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height / 2,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: ListView.builder(
-                        itemCount: controller.lista.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            margin: const EdgeInsets.symmetric(vertical: 4),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.black26,
-                            ),
-                            height: 100,
-                            width: 180,
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 4,
-                                    vertical: 4,
-                                  ),
-                                  child: InkWell(
-                                    onTap: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) => DetailsPage(
-                                                  results: controller
-                                                      .lista[index])));
-                                    },
-                                    child: Container(
-                                      height: 100,
-                                      width: 80,
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(20)),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(20),
-                                        child: Image.network(
-                                          controller.lista[index].cover
-                                              .toString(),
-                                          fit: BoxFit.fill,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 5.0, vertical: 4),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Container(
-                                        height: 40,
-                                        width: 170,
-                                        child: Text(controller
-                                            .lista[index].title
-                                            .toString()),
-                                      ),
-                                      Container(
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(30),
-                                            color: Colors.black),
-                                        child: Text(controller
-                                            .lista[index].publisher
-                                            .toString()),
-                                      ),
-                                      const SizedBox(height: 10),
-                                      Padding(
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 2.0),
-                                        child: Row(
-                                          children: const [
-                                            Icon(Icons.star,
-                                                color: Colors.amber),
-                                            SizedBox(width: 8),
-                                            Text('4.9'),
-                                            SizedBox(width: 20),
-                                            Icon(
-                                              Icons.download,
-                                              color: Colors.blueAccent,
-                                            ),
-                                            Text('289k')
-                                          ],
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 8.0),
-                                  child: Row(
-                                    children: [],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }),
-                  ),
-                );
+                return gameList(context);
               }
           }
         });
+  }
+
+  gameList(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 2,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: ListView.builder(
+            itemCount: controller.lista.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.symmetric(vertical: 4),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.black26,
+                ),
+                height: 100,
+                width: 180,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 4,
+                        vertical: 4,
+                      ),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => DetailsPage(
+                                      results: controller.lista[index])));
+                        },
+                        child: Container(
+                          height: 100,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20)),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(
+                              controller.lista[index].cover.toString(),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 5.0, vertical: 4),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 40,
+                            width: 170,
+                            child:
+                                Text(controller.lista[index].title.toString()),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.black),
+                            child: Text(
+                                controller.lista[index].publisher.toString()),
+                          ),
+                          const SizedBox(height: 10),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 2.0),
+                            child: Row(
+                              children: const [
+                                Icon(Icons.star, color: Colors.amber),
+                                SizedBox(width: 8),
+                                Text('4.5'),
+                                SizedBox(width: 20),
+                                Icon(
+                                  Icons.download,
+                                  color: Colors.blueAccent,
+                                ),
+                                Text('289k')
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
