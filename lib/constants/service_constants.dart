d8818d00093d8713a111b6413a31a219c2a82130
usuario <gomes.amilson@gmail.com>
2022-11-24 01:32:41 -0300

    criado arquivo service_constants.dart

---
+++ lib/constants/service_constants.dart
@@ -0,0 +1,6 @@
+class ServiceConstants{
+  static const baseUrl ='https://videogamesapi.herokuapp.com/api/games/';
+  static const baseUrlPage3 ='https://videogamesapi.herokuapp.com/api/games/?page=3';
+  static const baseUrlPage2 ='https://videogamesapi.herokuapp.com/api/platforms/?page=2';
+
+}
