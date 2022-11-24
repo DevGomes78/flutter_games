b020ac8a4ee5869752e5fda8ee1c395e6c12455c
usuario <gomes.amilson@gmail.com>
2022-11-24 01:34:24 -0300

    atualizado string_constants.dart

--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -1,4 +1,7 @@
 class StringConstants{
   static const errorPage = 'erro na pagina';
-
+ static const  browseGames = 'Browse Games';
+ static const overwatchTournament ='overwatchTournament';
+ static const description = 'Description';
+ static const topThisYear ='Top This Year';
 }
