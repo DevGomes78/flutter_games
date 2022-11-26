4e24237ec54f67dbe39949bb7f03f448da043ee4
usuario <gomes.amilson@gmail.com>
2022-11-26 04:33:03 -0300

    atualizao codigo

--- lib/constants/service_constants.dart
+++ lib/constants/service_constants.dart
@@ -1,6 +1,6 @@
 class ServiceConstants{
-  static const baseUrl ='https://videogamesapi.herokuapp.com/api/games/';
-  static const baseUrlPage3 ='https://videogamesapi.herokuapp.com/api/games/?page=3';
-  static const baseUrlPage2 ='https://videogamesapi.herokuapp.com/api/platforms/?page=2';
-
+  static const urlgames1 ='https://videogamesapi.herokuapp.com/api/games/';
+  static const urlgames2 ='https://videogamesapi.herokuapp.com/api/games/?page=3';
+  static const urlgames3 ='https://videogamesapi.herokuapp.com/api/platforms/?page=2';
+  static const urlScreen ='https://videogamesapi.herokuapp.com/api/screenshots/';
 }
