63df68a46ea899b4244ccb3998becec8f9b23212
usuario <gomes.amilson@gmail.com>
2022-11-26 04:33:50 -0300

    atualizao codigo

--- lib/service/games_service.dart
+++ lib/service/games_service.dart
@@ -1,34 +1,35 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_games/models/game_model.dart';
 import 'package:http/http.dart' as http;
-
 import '../constants/service_constants.dart';
 
 class GameService extends ChangeNotifier {
   List<Results> lista =[];
   List<Results> lista1 =[];
   List<Results> lista2 =[];
+ var json;
+
   Future<List<Results>> getData() async {
     final response = await http
-        .get(Uri.parse(ServiceConstants.baseUrl));
+        .get(Uri.parse(ServiceConstants.urlgames1));
      var decodejson = jsonDecode(response.body);
      decodejson['results'].forEach((item)=> lista.add(Results.fromJson(item)));
      return lista;
   }
   Future<List<Results>> getData1() async {
     final response = await http
-        .get(Uri.parse(ServiceConstants.baseUrlPage3));
+        .get(Uri.parse(ServiceConstants.urlgames2));
     var decodejson = jsonDecode(response.body);
     decodejson['results'].forEach((item)=> lista1.add(Results.fromJson(item)));
     return lista1;
   }
-  Future<List<Results>> getConsole() async {
+
+  Future<void> getScreen() async {
     final response = await http
-        .get(Uri.parse(ServiceConstants.baseUrlPage2));
-    var decodejson = jsonDecode(response.body);
-    decodejson['results'].forEach((item)=> lista2.add(Results.fromJson(item)));
-    return lista2;
+        .get(Uri.parse('https://videogamesapi.herokuapp.com/api/screenshots/'));
+    json = jsonDecode(response.body);
+    return json;
   }
+
 }
